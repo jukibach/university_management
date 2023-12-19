51814f090ee47c32efd205d251e9169645493163
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-12-19 16:48:01 +0700

    Updated AccountControllerTest

--- src/test/java/com/example/university_management/controller/AccountControllerTest.java
+++ src/test/java/com/example/university_management/controller/AccountControllerTest.java
@@ -37,6 +37,8 @@ class AccountControllerTest {
     void testGetAllAccounts() throws Exception {
         Account account1 = new Account(); // Assuming Account is a valid class
         Account account2 = new Account();
+        Account account3 = new Account();
+        
         // ... Initialize account objects
         List<Account> accounts = Arrays.asList(account1, account2);
         
