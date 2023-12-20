d959713a0f66e888863d4c36a2249c797f7a7379
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-12-20 13:38:00 +0700

    Fixed CORS

--- src/main/java/com/example/university_management/config/WebSecurityConfig.java
+++ src/main/java/com/example/university_management/config/WebSecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +21,22 @@ public class WebSecurityConfig {
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        CorsConfiguration configuration = new CorsConfiguration();
+        List<String> all = Collections.singletonList("*");
+        configuration.setAllowedOrigins(all);
+        configuration.setAllowedMethods(all);
+        configuration.setAllowedHeaders(all);
+        configuration.setAllowCredentials(false);
+        
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
     
     @Bean
