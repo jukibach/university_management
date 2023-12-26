56cdfd126049dae4c89fe4dc607c036ce4b04e5e
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-12-26 08:22:02 +0700

    Added more details for OpenAPI specification

--- src/main/java/fpt/com/universitymanagement/dto/LoginRequest.java
+++ src/main/java/fpt/com/universitymanagement/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package fpt.com.universitymanagement.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @Setter
 public class LoginRequest {
     @NotBlank(message = "User name must not be null")
+    @Min(message = "User name must have at least 8 characters", value = 8)
     private String userName;
+    
     @NotBlank(message = "Password must not be null")
+    @Min(message = "Password must have at least 8 characters", value = 8)
     private String password;
 }
