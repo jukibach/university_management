1c5eae36c60cb5d83c31810e2813d852cc9ca06d
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-12-27 15:48:34 +0700

    Implemented redis and refactored code

--- src/main/java/fpt/com/universitymanagement/dto/RefreshTokenRequest.java
+++ src/main/java/fpt/com/universitymanagement/dto/RefreshTokenRequest.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class TokenRefreshRequest {
+public class RefreshTokenRequest {
     @NotBlank(message = "Token must be non-null")
-    private String refreshToken;
+    private String accessToken;
 }
