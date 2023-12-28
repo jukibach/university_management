ff043eb95476752ae88b20529a5df310614d5991
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-12-28 12:56:48 +0700

    Added logout feature

--- src/main/java/fpt/com/universitymanagement/UniversityManagementApplication.java
+++ src/main/java/fpt/com/universitymanagement/UniversityManagementApplication.java
@@ -1,10 +1,8 @@
 package fpt.com.universitymanagement;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("fpt.com.universitymanagement.mapper")
 @SpringBootApplication
 public class UniversityManagementApplication {
     
--- src/main/java/fpt/com/universitymanagement/mapper/AccountMapper.java
+++ src/main/java/fpt/com/universitymanagement/mapper/AccountMapper.java
@@ -5,18 +5,20 @@ import fpt.com.universitymanagement.entity.account.Account;
 import fpt.com.universitymanagement.entity.account.RoleAccount;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AccountMapper {
+    @Named("mapRoleAccounts")
     default Set<String> mapRoleAccounts(Set<RoleAccount> roleAccounts) {
         return roleAccounts.stream()
                 .map(roleAccount -> roleAccount.getRole().getName())
                 .collect(Collectors.toSet());
     }
     
-    @Mapping(target = "roleAccounts", source = "roleAccounts")
+    @Mapping(target = "roleAccounts", qualifiedByName = "mapRoleAccounts")
     AccountResponse accountToAccountResponse(Account account);
 }
