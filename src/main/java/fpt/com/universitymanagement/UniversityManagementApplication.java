9a387fe80bb4b90fc01b23ebc5628072590b30c2
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2024-01-27 11:27:27 +0700

    Jenkins demo

--- src/main/java/fpt/com/universitymanagement/UniversityManagementApplication.java
+++ src/main/java/fpt/com/universitymanagement/UniversityManagementApplication.java
@@ -1,12 +1,15 @@
 package fpt.com.universitymanagement;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class UniversityManagementApplication {
     
     public static void main(String[] args) {
+        log.info("Application start");
         SpringApplication.run(UniversityManagementApplication.class, args);
     }
     
